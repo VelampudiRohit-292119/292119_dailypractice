3100641906cdc1002531289ff26cd10b80a5cdc3
Velampudi Rohit <velampudirohit1605@gmail.com>
2021-04-22 23:01:40 +0530

    added more work

--- Python-Examples/Pythonexamples/if_elif_else condition/unsolved03.py
+++ Python-Examples/Pythonexamples/if_elif_else condition/unsolved03.py
@@ -5,4 +5,18 @@
 #- complex number
 #- Zero (0)
 num=input("Enter the number:")
+if(num=='0'):
+    print("Zero")
+elif(num.find('+')!=-1):
+    print("complex")
+elif(num.isalnum()==True):
+    print("string")
+elif(num.find('+')==-1):
+    if(num.find('.')>=1):
+        print("float")
+    else:
+        print("real")
+else:
+    print("None of wanted")
+    
 
--- Python-Examples/Pythonexamples/set/unsolved01.py
+++ Python-Examples/Pythonexamples/set/unsolved01.py
@@ -1,3 +1,4 @@
 #Write a Python program to find maximum and the minimum value in a set
 sets=set([4,5,6,7,8,1,2,3,14,0])
+print(max(sets))
 print(min(sets))
